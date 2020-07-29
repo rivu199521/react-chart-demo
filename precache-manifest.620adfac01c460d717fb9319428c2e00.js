b43e447a1bf553420e743ab2fbab6bc071b79be1
unknown <mukherjeerivu7@gmail.com>
2020-07-29 10:27:44 +0530

    Updates

---
+++ precache-manifest.620adfac01c460d717fb9319428c2e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1614834fb4287f179683df07b6f2634a",
+    "url": "/react-chart-demo/index.html"
+  },
+  {
+    "revision": "114a06fb8474b4e7fb72",
+    "url": "/react-chart-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f37400a36c3f9f623b2",
+    "url": "/react-chart-demo/static/js/2.3dd573b2.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react-chart-demo/static/js/2.3dd573b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "114a06fb8474b4e7fb72",
+    "url": "/react-chart-demo/static/js/main.7a0039b6.chunk.js"
+  },
+  {
+    "revision": "bdd1fdbe84274adba599",
+    "url": "/react-chart-demo/static/js/runtime-main.473e8739.js"
+  }
+]);
